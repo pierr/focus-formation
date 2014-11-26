56e170369df0610d55b566f8e431a7984fbb6fdb
Pierre <pierre.besson7@gmail.com>
2014-11-27 00:39:40 +0100

    Handle the line selection.

--- app/views/user/userRecherche/userResults.js
+++ app/views/user/userRecherche/userResults.js
@@ -6,6 +6,15 @@ var resultsTemplate = require('./templates/userResults');
  * @module "views/message/messageRecherche"
  */
 module.exports = ResultsView.extend({
+  events:{
+    "click div[data-id]": "handleLineClick"
+  },
+  //Gestion du click sur la ligne.
+  handleLineClick: function handleLineClick(evt){
+    var id = $(event.target).closest('[data-id]').attr('data-id');
+    //event.target.getAttribute('data-id');
+    Backbone.history.navigate('user/'+id, true);
+  },
   /**
    * Template in order to display all the results.
    * @type {function}
